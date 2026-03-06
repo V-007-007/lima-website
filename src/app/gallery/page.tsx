f342b12cdc51e9138556e034f85e39fb5e547688
V-007-007 <irismatt00@gmail.com>
2026-03-06 03:15:59 +0100

    Enhance GalleryPage with more images and layout
    Updated gallery images with additional entries and improved layout.

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -7,7 +7,15 @@ export default function GalleryPage() {
   const galleryImages = [
     {
       id: '1',
-      src: ['/images/jpegs/DSC_0991.jpg', '/images/jpegs/DSC_1009.jpg', '/images/jpegs/DSC_1014.jpg'],
+      src: ['/images/jpegs/DSC_0991.jpg', '/images/jpegs/DSC_1009.jpg', '/images/jpegs/DSC_1014.jpg', '/images/jpegs/DSC_1028.jpg', '/images/jpegs/DSC_1030.jpg', '/images/jpegs/DSC_1041.jpg', '/images/jpegs/DSC_1042.jpg', '/images/jpegs/DSC_1050.jpg', 
+           '/images/jpegs/DSC_1051.jpg', '/images/jpegs/DSC_1052.jpg', '/images/jpegs/DSC_1053.jpg', '/images/jpegs/DSC_1056.jpg', '/images/jpegs/DSC_1060.jpg', '/images/jpegs/DSC_1064.jpg', '/images/jpegs/DSC_1066.jpg', '/images/jpegs/DSC_1069.jpg', 
+           '/images/jpegs/DSC_1074.jpg', '/images/jpegs/DSC_1076.jpg', '/images/jpegs/DSC_1078.jpg', '/images/jpegs/DSC_1079.jpg', '/images/jpegs/DSC_1081.jpg', '/images/jpegs/DSC_1087.jpg', '/images/jpegs/DSC_1091.jpg', '/images/jpegs/DSC_1092.jpg', 
+           '/images/jpegs/DSC_1094.jpg', '/images/jpegs/DSC_1097.jpg', '/images/jpegs/DSC_1102.jpg', '/images/jpegs/DSC_1107.jpg', '/images/jpegs/DSC_1113.jpg', '/images/jpegs/DSC_1129.jpg', '/images/jpegs/DSC_1131.jpg', '/images/jpegs/DSC_1133.jpg', 
+           '/images/jpegs/DSC_1138.jpg', '/images/jpegs/DSC_1139.jpg', '/images/jpegs/DSC_1131.jpg', '/images/jpegs/DSC_1142.jpg', '/images/jpegs/DSC_1148.jpg', '/images/jpegs/DSC_1161.jpg', '/images/jpegs/DSC_1166.jpg', '/images/jpegs/DSC_1169.jpg', 
+           '/images/jpegs/DSC_1170.jpg', '/images/jpegs/DSC_1177.jpg', '/images/jpegs/DSC_1182.jpg', '/images/jpegs/DSC_1186.jpg', '/images/jpegs/DSC_1190.jpg', '/images/jpegs/DSC_1191.jpg', '/images/jpegs/DSC_1193.jpg', '/images/jpegs/DSC_1199.jpg', 
+           '/images/jpegs/DSC_1208.jpg', '/images/jpegs/DSC_1208.jpg', '/images/jpegs/DSC_1210.jpg', '/images/jpegs/DSC_1212.jpg', '/images/jpegs/DSC_1214.jpg', '/images/jpegs/DSC_1216.jpg', '/images/jpegs/DSC_1223.jpg', '/images/jpegs/DSC_1232.jpg', 
+           '/images/jpegs/DSC_1233.jpg', '/images/jpegs/DSC_1241.jpg', '/images/jpegs/DSC_1242.jpg', '/images/jpegs/DSC_1242.jpg', '/images/jpegs/DSC_1243.jpg', '/images/jpegs/DSC_1244.jpg', '/images/jpegs/DSC_1246.jpg', '/images/jpegs/DSC_1247.jpg', 
+           '/images/jpegs/DSC_1248.jpg', '/images/jpegs/DSC_1264.jpg', '/images/jpegs/DSC_1266.jpg'],
       alt: 'LIMA Graduation Ceremony 2023',
       title: 'Graduation Ceremony 2023',
     },
@@ -87,5 +95,6 @@ export default function GalleryPage() {
   )
 
 }
+
 
 
